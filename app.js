8a2f3b8d0941fec1192b54dd20e52ec03d7ddd74
Taniacardenas <tania.cardenas103@gmail.com>
2021-09-19 19:38:35 -0500

    Se Modifica el case listar y se agrago un case crear

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+const tareas = require("./tareas.json")
 const logic = require ("./funcionesTareas.js")
 
 
 let opciones = process.argv[2]
-logic(opciones)
-// console.log(tareas)
+let genero = process.argv[3]
+
+switch (opciones) {
+    case "listar":
+        tareas.forEach((item, index)=>{ 
+            console.log(tareas[index])
+        })
+        break;
+    case "crear":
+        logic.crearTarea(genero)
+        break;
+
+    case undefined:
+        console.log("Atención - Tienes que pasar una acción")
+        break;
+
+    default:
+        console.log("No entiendo qué quieres hacer.")
+        break;
+}
 
